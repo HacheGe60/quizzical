b10aa4c983a4cc3ed77375d509dbc343c9e89bc3
lolodev <horaciodev1706@gmail.com>
2025-09-02 16:33:23 -0300

    intermezzo

--- Components/FirstPage/FirstPage.jsx
+++ Components/FirstPage/FirstPage.jsx
@@ -1,9 +1,75 @@
 export default function FirstPage() {
+
+  function getQuestions() {
+    const firstPageEl = document.getElementById('first-page');
+    firstPageEl.classList.add('hidden');
+
+    return (
+      <>
+
+      </>
+    );
+  }
+
   return (
     <>
-      <h1 className="text-[2rem] text-[var(--color-title)] font-bold">Quizzical</h1>
-      <p className="text-[1rem] text-[var(--color-title)] mb-6">Lightweight trivia game</p>
-      <button className="bg-[var(--bg-main-btn)] text-[var(--color-main-btn)] py-2 px-12 rounded-xl">Start quiz</button>
+      <div className="flex flex-col justify-center items-center" id="first-page">
+        <h1 className="text-[2rem] text-[var(--color-main)] font-bold">Quizzical</h1>
+        <p className="text-[1rem] text-[var(--color-main)] mb-6">Lightweight trivia game</p>
+        <button className="bg-[var(--bg-main-btn)] text-[var(--color-main-btn)] py-2 px-12 rounded-xl" onClick={() => getQuestions()}>Start quiz</button>
+      </div>
+      <div id="questions" className="flex flex-col justify-between items-between h-screen">
+        <div>
+          <p className="text-[var(--color-main)] font-bold text-base mb-2">How would one say goodbye in Spanish?</p>
+          <div className="flex justify-center items-center gap-4 mb-4">
+            <button className="text-[0.8rem] border-1 border-[var(--color-main)] rounded-md  px-4 hover:bg-[#d6dbf5] active:[#d6dbf5]">Adiós</button>
+            <button className="text-[0.8rem] border-1 border-[var(--color-main)] rounded-md  px-4 hover:bg-[#d6dbf5] active:[#d6dbf5]">Hola</button>
+            <button className="text-[0.8rem] border-1 border-[var(--color-main)] rounded-md  px-4 hover:bg-[#d6dbf5] active:[#d6dbf5]">Au Revoir</button>
+            <button className="text-[0.8rem] border-1 border-[var(--color-main)] rounded-md  px-4 hover:bg-[#d6dbf5] active:[#d6dbf5]">Salir</button>
+          </div>
+          <hr></hr>
+        </div>
+        <div>
+          <p>How would one say goodbye in Spanish?</p>
+          <div className="flex justify-center items-center gap-4">
+            <button>Adios</button>
+            <button>Hola</button>
+            <button>Au Revoir</button>
+            <button>Salir</button>
+          </div>
+          <hr></hr>
+        </div>
+        <div>
+          <p>How would one say goodbye in Spanish?</p>
+          <div className="flex justify-center items-center gap-4">
+            <button>Adios</button>
+            <button>Hola</button>
+            <button>Au Revoir</button>
+            <button>Salir</button>
+          </div>
+          <hr></hr>
+        </div>
+        <div>
+          <p>How would one say goodbye in Spanish?</p>
+          <div className="flex justify-center items-center gap-4">
+            <button>Adios</button>
+            <button>Hola</button>
+            <button>Au Revoir</button>
+            <button>Salir</button>
+          </div>
+          <hr></hr>
+        </div>
+        <div>
+          <p>How would one say goodbye in Spanish?</p>
+          <div className="flex justify-center items-center gap-4">
+            <button>Adios</button>
+            <button>Hola</button>
+            <button>Au Revoir</button>
+            <button>Salir</button>
+          </div>
+          <hr></hr>
+        </div>
+      </div>
     </>
   );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,9 @@ function App() {
 
   return (
     <>
-      <main class="relative w-full h-full flex justify-center items-center">
+      <main class="relative w-screen h-screen flex justify-center items-center">
         <div class="absolute -top-120 -right-120 h-64 w-64 bg-[#fff6b8] rounded-[55%_70%_45%_65%]"></div>
-        <div className="flex flex-col justify-center items-center w-full h-full">
-          <FirstPage />
-        </div>
+        <FirstPage />
         <div class="absolute -bottom-120 -left-120 h-64 w-64 bg-[#dfeaff] rounded-[65%_45%_70%_55%]"></div>
       </main>
 
